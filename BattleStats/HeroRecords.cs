06da1db81286a5bf401444a75737d739de5df135
JP <76245535+jzeno@users.noreply.github.com>
2022-09-18 03:22:00 -0400

    For M&B II Bannerlord e1.8.0

---
+++ BattleStats/HeroRecords.cs
@@ -0,0 +1,12 @@
+﻿namespace BattleStats
+{
+    public class HeroRecords
+    {
+        public string Name;
+        public int Kills;
+        public int PR;
+        public double KB;
+        public int Wounds;
+        public int Battles;
+    }
+}
